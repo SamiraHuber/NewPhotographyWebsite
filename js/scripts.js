9f373a0d5906df85997fa10ebeea75cb1429e2a1
Samira Huber <mail@samira-huber.de>
2020-12-05 10:01:39 +0100

    add tipps and tricks to portratis

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,17 @@ function toggleInfoContainer() {
         button.innerHTML = "&#8593; Weniger Informationen zum Angebot";
     }
 }
+
+function toggleTippsContainer() {
+    const el = document.getElementById("more_tipps");
+    const button = document.getElementById("more_info_button");
+    if (el.style.display == "block") {
+        el.style.display = "none";
+        button.innerHTML = "&#8594; Zeig mir Tipps f&uuml;r das Photoshooting";
+    }
+    else {
+        el.style.display = "block";
+        button.innerHTML = "&#8593; Weniger Tipps f&uuml;r das Photoshooting";
+    }
+}
+
